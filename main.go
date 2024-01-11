7c9cfb2c5cf8f53570f0fe75efb05fa50007cd7f
dilan <125643732+therealdilan@users.noreply.github.com>
2024-01-11 19:27:53 +1100

    'new' command mostly done

--- main.go
+++ main.go
@@ -6,30 +6,65 @@ import (
   "os"
 )
 
-type newYank struct {
-  Command string
-  Id string
+type Yank struct {
+  Command string `json:"command"`
 }
 
 func main() {
-  var n = newYank{}
+  cmd := os.Args[1]
+  cmdId := os.Args[2]
+  cmdCommand := os.Args[3]
 
-  fmt.Scanln(&n.Command)
-  fmt.Scanln(&n.Id)
+  yanks := make(map[string]Yank)
 
-  fmt.Println(n)
+  switch cmd {
+    case "new":
+      yanks[cmdId] = Yank{Command: cmdCommand}      
+      yankBytes, err := json.Marshal(yanks)
+      if err != nil {
+        panic(err)
+      }
 
-  content, err := json.Marshal(n)
-  if err != nil {
-    fmt.Println(err)
-    return
+      file, err := os.OpenFile("yanks.json", os.O_WRONLY|os.O_TRUNC, 0644)
+        if err != nil {
+          fmt.Println("Error opening file:", err)
+          return
+      }
+      defer file.Close()
+
+      encoder := json.NewEncoder(file)
+      err = encoder.Encode(yankBytes)
+      if err != nil {
+        fmt.Println("Error encoding JSON:", err)
+        return
+      }
+      break;
   }
+}
 
-  fmt.Println(string(content))
+func getYanks() map[string]Yank {
+  _, err := os.Stat("yanks.json")
 
-  err = os.WriteFile("yank.json",content,0644)
-  if err != nil {
+  if err == nil {
+    existingYanks, err := os.ReadFile("yanks.json")
+    yanks := make(map[string]Yank)
+
+    fmt.Println(existingYanks)
+
+    err = json.Unmarshal(existingYanks, &yanks)
+
+    if err != nil {
+      panic(err)
+    }
+
+    fmt.Println(yanks)
+
+    return yanks 
+  } else if os.IsNotExist(err) {
+      os.Create("yanks.json")
+      return nil
+  } else {
     fmt.Println(err)
-    return
   }
-} 
+  return nil
+}
